4bba315ff96a62e0a6b2f4b3a431b82e5665dd86
Shital Shah <shitals@microsoft.com>
2023-08-11 11:04:35 +0000

    bug went away when measuring equi distance epoch

--- grokking/train.py
+++ grokking/train.py
@@ -11,37 +11,40 @@ from grokking.logger import Logger, DEFAULT_WANDB_METRICS
 from grokking import utils
 from grokking.utils import ExponentialMovingAverage, SmoothedDyDx
 
-s1=0
-exp_name = 'log_p50-1_e500'
-def evaluate(model, val_loader, device, criterion)->Tuple[float, float]:
+
+skip_first, skip_interval, on_epoch_step = 5, 10, 2  # 1, 2 is low score, 2, 2 # high score
+exp_name = f'log_fs{skip_first}_ee{skip_interval}_es{on_epoch_step}_b669'
+
+def evaluate(model, val_loader, device, criterion,
+             step, epoch_step, samples, epochs, num_steps)->Tuple[float, float]:
+
     correct = 0
     loss_sum = 0.
     loss, acc = 0., 0.
 
-    global s1
-    s1+=1
-
-    pass1, pass2 = 50, 1 # 1, 2 is low score, 2, 2 # high score
+    if (step+1)<num_steps:
+        if step-skip_first <= 0:
+            return -1, -1
+        if step-skip_first!=1 and (epoch_step != on_epoch_step or epochs % skip_interval != 0):
+            return -2, -2
 
     # Set model to evaluation mode
     model.eval()
 
     with torch.no_grad():
-        if s1>=pass1:
             # Loop over each batch from the validation set
             for batch in val_loader:
-                if s1>=pass2:
-                    inputs, labels = tuple(t.to(device) for t in batch)
-                    #inputs, labels = torch.unsqueeze(inputs,0), torch.unsqueeze(labels,0)
+                inputs, labels = tuple(t.to(device) for t in batch)
+                #inputs, labels = torch.unsqueeze(inputs,0), torch.unsqueeze(labels,0)
 
-                    # a=inputs[:,1]
-                    # b=inputs[:,3]
-                    # assert(sum(((labels-11)*(b-11) % 223)==(a-11))==(len(labels)))
+                # a=inputs[:,1]
+                # b=inputs[:,3]
+                # assert(sum(((labels-11)*(b-11) % 223)==(a-11))==(len(labels)))
 
-                    output = model(inputs)[-1,:,:]
-                    correct += (torch.argmax(output, dim=1) == labels).sum().item()
-                    loss_sum += criterion(output, labels).item() * len(labels)
-            #assert count == len(val_loader.dataset)
+                output = model(inputs)[-1,:,:]
+                correct += (torch.argmax(output, dim=1) == labels).sum().item()
+                loss_sum += criterion(output, labels).item() * len(labels)
+                #assert count == len(val_loader.dataset)
 
     loss = loss_sum / len(val_loader.dataset)
     acc = correct / len(val_loader.dataset)
@@ -129,6 +132,7 @@ def train(config:Mapping):
     )
 
     step, start_time = 0, time.time()
+    samples, epochs, epoch_step = 0, 0, 0
     ewa_train_loss, ewa_val_loss = ExponentialMovingAverage(weight=0.1), ExponentialMovingAverage(weight=0.1)
     w_norm_ewa = ExponentialMovingAverage(weight=0.3)
     d_train_loss = SmoothedDyDx(y_ema_weight=1.0, x_ema_weight=0.5, dx_ema_weight=0.9,
@@ -138,9 +142,12 @@ def train(config:Mapping):
     criterion = torch.nn.CrossEntropyLoss()
     model.train()
     while step < num_steps:
+        epoch_step = 0
         # Loop over each batch from the training set
         for batch in train_loader:
             inputs, labels = tuple(t.to(device) for t in batch)
+
+            samples += len(labels)
 
             optimizer.zero_grad()
 
@@ -162,28 +169,23 @@ def train(config:Mapping):
             ewa_train_loss.add(loss.item())
             d_train_loss.add(loss.item(), step)
 
-            if step % 20 == 0 or step+1 >= num_steps:
-                metrics = {
-                    "train/step": step,
-                    "train/acc": acc.item(),
-                    "train/loss": loss.item(),
-                    "train/ewa_loss": ewa_train_loss.value,
-                    "train/d_loss": d_train_loss.value,
-                }
+            # if step % 20 == 0 or step+1 >= num_steps:
+            #     metrics = {
+            #         "train/step": step,
+            #         "train/acc": acc.item(),
+            #         "train/loss": loss.item(),
+            #         "train/ewa_loss": ewa_train_loss.value,
+            #         "train/d_loss": d_train_loss.value,
+            #     }
                 #logger.info(metrics)
 
-            if step % eval_every == 0 or step+1 >= num_steps:
-                val_loss, val_acc = evaluate(model, val_loader, device, criterion)
-
-                # ewa_val_loss.add(val_loss)
-                # d_val_loss.add(val_loss, step)
-
-                # w_norm = model.weight_norm()
-                # w_norm_ewa.add(w_norm)
-
+            val_loss, val_acc = evaluate(model, val_loader, device, criterion,
+                                            step, epoch_step, samples, epochs, num_steps)
+            if val_acc > 0:
                 val_metrics = {
                     "train/step": step,
                     "val/acc": val_acc,
+                    "train/epoch_step": epoch_step,
                     # "val/loss": val_loss,
                     # "w_norm": w_norm,
                     # "w_norm_ewa": w_norm_ewa.value,
@@ -194,9 +196,13 @@ def train(config:Mapping):
                 }
                 logger.info(val_metrics)
 
+
             step += 1
+            epoch_step += 1
             if step >= num_steps:
                 break
+        epochs += 1
+
 
     logger.summary({"train_time_hr": (time.time() - start_time)/3600,
                     "step_time_s": (time.time() - start_time)/step,})
