626f3c0a09dffe1fc25d6df452dcc17ca340695e
Shital Shah <sytelus@gmail.com>
2023-10-01 23:44:14 -0700

    dist test

--- scripts/play.py
+++ scripts/play.py
@@ -1,3 +1,4 @@
+# torchrun --standalone --nproc_per_node=8 scripts/play.py
 import math
 import inspect
 from dataclasses import dataclass
@@ -11,4 +12,14 @@ if __name__ == "__main__":
     dist.init_process_group(backend='nccl', init_method='env://')
     print("Hello from rank {}".format(dist.get_rank()))
 
+    t = torch.ones(1, device='cuda:{}'.format(dist.get_rank()))
+    print("Rank {} has {}".format(dist.get_rank(), t))
+
+    dist.barrier()
+
+    dist.reduce(t, 0)
+
+    print("Rank {} has {}".format(dist.get_rank(), t))
+
+    dist.destroy_process_group()
 
