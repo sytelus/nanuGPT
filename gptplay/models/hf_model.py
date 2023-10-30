e32b7f08a797a0f13fe8ba08068ad111b637b170
Shital Shah <shitals@microsoft.com>
2023-10-30 10:54:07 -0700

    add more params for hf_model

--- gptplay/models/hf_model.py
+++ gptplay/models/hf_model.py
@@ -1,12 +1,16 @@
+from typing import List
 from gptplay import utils
 from gptplay import glogging as logging
 
+def compute_intermediate_size(n, ffn_dim_multiplier=1, multiple_of=256):
+    return multiple_of * ((int(ffn_dim_multiplier * int(8 * n / 3)) + multiple_of - 1) // multiple_of)
 
+# for llama: sample values are at https://huggingface.co/EleutherAI/llemma_7b/blob/main/config.json
 def get_model(
-              n_layer: int, n_embd: int, n_head: int,
-              vocab_size: int, context_length: int,
               config_class_name: str, # 'transformers.models.llama.LlamaConfig'
               model_class_name: str,  # 'transformers.models.llama.LlamaForCausalLM'
+              n_layer: int, n_embd: int, n_head: int,
+              vocab_size: int, context_length: int,
               use_gqa: bool, # False for  < 13B
               tie_word_embeddings: bool, # kept false for supporting parallelization
               rope_theta: float, # theta for RoPE, 10000.0 but should be 10*contex_len
@@ -14,6 +18,11 @@ def get_model(
               eos_token_id: int, # 2
               pad_token_id: int, # 0
               use_flash_Attn2: bool,
+              torch_dtype: str, # 'float16'
+
+              # below two are probably not needed
+              architectures: List[str], # ["LlamaForCausalLM"]
+              model_type: str, # 'llama'
               ):
 
     model_config_cls = utils.import_fn(config_class_name)
@@ -22,7 +31,7 @@ def get_model(
         hidden_size=n_embd,
         num_hidden_layers=n_layer,
         num_attention_heads=n_head,
-        intermediate_size=n_embd * 4,
+        intermediate_size=compute_intermediate_size(n_embd),
         num_key_value_heads=n_head if not use_gqa else 1,
         max_position_embeddings = context_length,
         use_cache=True,
@@ -31,6 +40,8 @@ def get_model(
         bos_token_id = bos_token_id,
         eos_token_id = eos_token_id,
         pad_token_id = pad_token_id,
+        torch_dtype = torch_dtype,
+        model_type = model_type,
     )
 
     flash_attn_used = use_flash_Attn2 and utils.flash_attn_supported()
