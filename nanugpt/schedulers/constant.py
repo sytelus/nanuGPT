e29b9c94bdb7f7d105a0066f44b6f91344119f01
Shital Shah <sytelus@gmail.com>
2023-11-11 15:57:09 +0000

    use num_steps for const lr

--- nanugpt/schedulers/constant.py
+++ nanugpt/schedulers/constant.py
@@ -6,14 +6,14 @@ from torch.optim.lr_scheduler import LRScheduler
 from nanugpt import glogging as logging
 
 class ConstantWithCooldownScheduler(LRScheduler):
-    def __init__(self, optimizer, const_lr:float, warmup_iters: int, lr_const_iters: int, cooldown_iters: int,
+    def __init__(self, optimizer, const_lr:float, warmup_iters: int, num_steps: int, cooldown_iters: int,
                  last_epoch=-1, verbose=False):
 
         self.const_lr = const_lr
 
         self.warmup_iters = warmup_iters
         self.cooldown_iters = cooldown_iters
-        self.lr_const_iters = lr_const_iters
+        self.num_steps = num_steps
 
         super().__init__(optimizer, last_epoch, verbose)
 
@@ -28,20 +28,20 @@ class ConstantWithCooldownScheduler(LRScheduler):
         if self.last_epoch < self.warmup_iters:
             return (init_lrs * self.last_epoch / self.warmup_iters).tolist()
 
-        # 2) if it > lr_const_iters, return cooldown learning rate
-        if self.last_epoch > self.lr_const_iters:
-            cooldown_start = self.last_epoch - self.lr_const_iters - self.warmup_iters
-            return (init_lrs * (1 - cooldown_start / self.cooldown_iters)).tolist()
+        # 2) if it > num_steps, return cooldown learning rate
+        if self.last_epoch > (self.num_steps - self.cooldown_iters):
+            cooldown_start = self.num_steps - self.last_epoch
+            return (init_lrs * (cooldown_start / self.cooldown_iters)).tolist()
 
         # 3) in between, use constant learning rate
         return np.full_like(init_lrs, self.const_lr).tolist()
 
 
-def get_scheduler(optimizer, const_lr:float, warmup_iters: int, lr_const_iters: int, cooldown_iters: int):
+def get_scheduler(optimizer, const_lr:float, warmup_iters: int, num_steps: int, cooldown_iters: int):
     return ConstantWithCooldownScheduler(
         optimizer=optimizer,
             const_lr=const_lr,
             warmup_iters=warmup_iters,
-            lr_const_iters=lr_const_iters,
+            num_steps=num_steps,
             cooldown_iters=cooldown_iters
         )
