cc25845a6592b62512c9ea66ec8331b9a3e40db1
Shital Shah <shitals@microsoft.com>
2023-12-16 16:10:21 -0800

    lr range test

---
+++ lr_range_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#fail if any errors
+set -e
+set -o xtrace
+
+python train.py {$1};configs/hp_tuning/lr_search.yaml
---
+++ nanugpt/schedulers/lr_range_test.py
@@ -0,0 +1,31 @@
+
+
+import math
+import numpy as np
+
+from torch.optim.lr_scheduler import LRScheduler
+
+from nanugpt import glogging as logging
+
+class LRRangeTestScheduler(LRScheduler):
+    def __init__(self, optimizer, max_steps:int, range_coeff:float=5.0,
+                 last_epoch=-1, verbose=False):
+        self.max_steps = max_steps
+        self.range_coeff = range_coeff
+        super().__init__(optimizer, last_epoch, verbose)
+
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            logging.warn("To get the last learning rate computed by the scheduler, please use `get_last_lr()`.")
+
+        # get initial LR set in each group in optimizer
+        init_lrs = np.fromiter((group['initial_lr'] for group in self.optimizer.param_groups), dtype=np.float32)
+
+        x = self.last_epoch / self.max_steps  # normalize to 0..1
+        c = math.exp(self.range_coeff * (x-0.5))
+        return (init_lrs * c).tolist()
+
+
+def get_scheduler(optimizer, max_steps: int, range_coeff: float):
+    return LRRangeTestScheduler(
+        optimizer=optimizer, max_steps=max_steps, range_coeff=range_coeff)
