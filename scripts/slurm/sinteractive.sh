936f1f2b867c2387c47287b7b511f6fb5bf614c8
Shital Shah <sytelus@gmail.com>
2024-12-20 04:26:25 -0800

    fix: remove unnecessary bash command wrapper from task epilog in SLURM interactive script

--- scripts/slurm/sinteractive.sh
+++ scripts/slurm/sinteractive.sh
@@ -66,5 +66,5 @@ srun ${PARTITION_ARG} ${RESERVATION_ARG} ${SHARE_NODE_ARG} ${NODELIST_ARG} \
     --container-mounts "${ALL_CONTAINER_MOUNTS}" \
     --container-writable --no-container-remap-root \
     --wait=60 --kill-on-bad-exit=1 \
-    --task-epilog="bash -c '/usr/bin/pkill -U \$SLURM_JOB_USER sshd'" \
+    --task-epilog="/usr/bin/pkill -U \$SLURM_JOB_USER sshd" \
     --pty /bin/bash -i
