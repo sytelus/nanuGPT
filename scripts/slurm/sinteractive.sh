292be5b491d96966e8175cabcf4c461b158739f9
Shital Shah <sytelus@gmail.com>
2024-12-21 04:49:35 -0800

    fix: update REQUIRED_VARS and set default for CONTAINER_IMAGE_PATH in sinteractive.sh for improved container configuration

--- scripts/slurm/sinteractive.sh
+++ scripts/slurm/sinteractive.sh
@@ -3,9 +3,10 @@
 set -eu -o xtrace -o pipefail # fail if any command failes, log all commands
 
 # required and optional variable
-REQUIRED_VARS=("GPUS_PER_NODE" "CONTAINER_IMAGE_PATH")
-CONTAINER_MOUNTS=${CONTAINER_MOUNTS:-"docker://\$oauthtoken@nvcr.io#nvidia/pytorch:24.07-py3"}  # app specific mounts to be attached to container as source:destination
+REQUIRED_VARS=()
+CONTAINER_MOUNTS=${CONTAINER_MOUNTS:-}  # app specific mounts to be attached to container as source:destination
 JOB_ENV_SETUP_SCRIPT=${JOB_ENV_SETUP_SCRIPT:-} # script to setup environment for specific cluster
+CONTAINER_IMAGE_PATH=${CONTAINER_IMAGE_PATH:-"docker://\$oauthtoken@nvcr.io#nvidia/pytorch:24.07-py3"}
 NODES=${NODES:-1}
 PARTITION=${PARTITION:-}
 RESERVATION=${RESERVATION:-}
