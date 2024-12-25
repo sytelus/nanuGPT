56400e550c55bd476c3ace2371ae556745f8b450
Shital Shah <shitals@microsoft.com>
2024-12-25 09:48:01 +0530

    manage zero_stage better

--- nanugpt/optimizers/adamw.py
+++ nanugpt/optimizers/adamw.py
@@ -3,7 +3,9 @@ import inspect
 import torch
 
 def get_optim(model, learning_rate, weight_decay,
-              beta1, beta2, eps, enable_fused):
+              beta1, beta2, eps, enable_fused, zero_stage):
+    assert zero_stage == 0, "ZeroRedundancyOptimizer is not supported with adamw.py"
+
     return torch.optim.AdamW(
         model.parameters(),
         lr=learning_rate,
