a805e71d5b93e98d5f686dae314854a75550ab70
Shital Shah <shitals@microsoft.com>
2024-12-25 17:00:53 +0530

    roll back to 24.07

--- scripts/slurm/sinteractive.sh
+++ scripts/slurm/sinteractive.sh
@@ -6,7 +6,7 @@ set -eu -o xtrace -o pipefail # fail if any command failes, log all commands
 CONTAINER_MOUNTS=${CONTAINER_MOUNTS:-}  # app specific mounts to be attached to container as source:destination
 JOB_ENV_SETUP_SCRIPT=${JOB_ENV_SETUP_SCRIPT:-} # script to setup environment for specific cluster
 set +u # protect against unbound var $oauthtoken which is not actually var here but convention
-CONTAINER_IMAGE_PATH=${CONTAINER_IMAGE_PATH:-"docker://@nvcr.io#nvidia/pytorch:24.12-py3"}
+CONTAINER_IMAGE_PATH=${CONTAINER_IMAGE_PATH:-"docker://@nvcr.io#nvidia/pytorch:24.07-py3"}
 set -u
 NODES=${NODES:-1}
 PARTITION=${PARTITION:-}
