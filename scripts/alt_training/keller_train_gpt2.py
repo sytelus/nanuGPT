91895f702712351bc2536e4737e4037ea3570580
Shital Shah <sytelus@gmail.com>
2025-01-12 12:02:23 -0800

    changed keller's script to use OWT

--- scripts/alt_training/keller_train_gpt2.py
+++ scripts/alt_training/keller_train_gpt2.py
@@ -1,4 +1,5 @@
 # from https://raw.githubusercontent.com/KellerJordan/modded-nanogpt/09a49d4af4804af92d14216b43136f5510a8fba8/train_gpt2.py
+# altered to use OpenWebText
 
 import os
 import sys
@@ -168,53 +169,18 @@ class GPT(nn.Module):
 
 # -----------------------------------------------------------------------------
 # Our own simple Distributed Data Loader
-
-def _peek_data_shard(filename):
-    # only reads the header, returns header data
-    with open(filename, "rb") as f:
-        # first read the header, which is 256 int32 integers (4 bytes each)
-        header = np.frombuffer(f.read(256*4), dtype=np.int32)
-    if header[0] != 20240520:
-        print("ERROR: magic number mismatch in the data .bin file!")
-        print("---> HINT: Are you passing in a correct file with --input_bin?")
-        print("---> HINT: Dataset encoding changed recently, re-run data prepro or refer again to README")
-        print("---> HINT: For example re-run: `python dev/data/tinyshakespeare.py`, then re-try")
-        exit(1)
-    assert header[1] == 1, "unsupported version"
-    ntok = header[2] # number of tokens (claimed)
-    return ntok # for now just return the number of tokens
-
-def _load_data_shard(filename):
-    with open(filename, "rb") as f:
-        # first read the header, which is 256 int32 integers (4 bytes each)
-        header = np.frombuffer(f.read(256*4), dtype=np.int32)
-        assert header[0] == 20240520, "magic number mismatch in the data .bin file"
-        assert header[1] == 1, "unsupported version"
-        ntok = header[2] # number of tokens (claimed)
-        # the rest of it are tokens, stored as uint16
-        tokens = np.frombuffer(f.read(), dtype=np.uint16)
-    assert len(tokens) == ntok, "number of tokens read does not match header?"
-    return tokens
-
 class DistributedDataLoader:
-    def __init__(self, filename_pattern, B, T, process_rank, num_processes):
+    def __init__(self, filepath, B, T, process_rank, num_processes):
         self.process_rank = process_rank
         self.num_processes = num_processes
         self.B = B
         self.T = T
 
-        # glob files that match the pattern
-        self.files = sorted(glob.glob(filename_pattern))
-        assert len(self.files) > 0, f"did not find any files that match the pattern {filename_pattern}"
+        self.tokens = np.array(np.memmap(filepath, dtype=np.uint16, mode="r"))
+        self.ntok_total = len(self.tokens)
 
-        # load and validate all data shards, count number of tokens in total
-        ntok_total = 0
-        for fname in self.files:
-            shard_ntok = _peek_data_shard(fname)
-            assert shard_ntok >= num_processes * B * T + 1
-            ntok_total += shard_ntok
-        self.ntok_total = ntok_total
-        print0(f"DataLoader: total number of tokens: {ntok_total:,} across {len(self.files)} files")
+        assert self.ntok_total > process_rank*B*T + B*T, f"dataset is too small for this process rank: {self.ntok_total} <= {process_rank*B*T + B*T + 1}, ntok_total={self.ntok_total}, process_rank={process_rank}, B={B}, T={T}"
+        print0(f"DataLoader: total number of tokens: {self.ntok_total:,}")
 
         # kick things off
         self.reset()
@@ -222,12 +188,10 @@ class DistributedDataLoader:
     def reset(self):
         self.current_shard = 0
         self.current_position = self.process_rank * self.B * self.T
-        self.tokens = _load_data_shard(self.files[self.current_shard])
 
     def advance(self): # advance to next data shard
-        self.current_shard = (self.current_shard + 1) % len(self.files)
+        self.current_shard = 0
         self.current_position = self.process_rank * self.B * self.T
-        self.tokens = _load_data_shard(self.files[self.current_shard])
 
     def next_batch(self):
         B = self.B
@@ -258,26 +222,30 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     # file system input / output
-    parser.add_argument("--input_bin", type=str, default="dev/data/tinyshakespeare/tiny_shakespeare_val.bin", help="input .bin to train on")
-    parser.add_argument("--input_val_bin", type=str, default="", help="input .bin to eval validation loss on")
-    parser.add_argument("--output_dir", type=str, default="", help="output directory to which to write logs and checkpoints")
-    parser.add_argument("--model", type=str, default="gpt2", help="gpt2|gpt2-medium|gpt2-large|gpt2-xl|d12|d24|d36|d48")
+    parser.add_argument("--input_bin", type=str, default="$DATA_ROOT/tokenized/openwebtext/tiktoken/train.bin", help="input .bin to train on")
+    parser.add_argument("--input_val_bin", type=str, default="$DATA_ROOT/tokenized/openwebtext/tiktoken/val.bin", help="input .bin to eval validation loss on")
+    parser.add_argument("--output_dir", type=str, default="$OUT_DIR", help="output directory to which to write logs and checkpoints")
+    parser.add_argument("--model", type=str, default="d12", help="gpt2|gpt2-medium|gpt2-large|gpt2-xl|d12|d24|d36|d48")
     # token layout for each step of the optimization
-    parser.add_argument("--batch_size", type=int, default=4, help="batch size, in units of #batch dimensions")
-    parser.add_argument("--sequence_length", type=int, default=64, help="sequence length")
-    parser.add_argument("--total_batch_size", type=int, default=256, help="total desired batch size, in units of #tokens")
+    parser.add_argument("--batch_size", type=int, default=64, help="batch size, in units of #batch dimensions")
+    parser.add_argument("--sequence_length", type=int, default=1024, help="sequence length")
+    parser.add_argument("--total_batch_size", type=int, default=524288, help="total desired batch size, in units of #tokens")
     # workload (number of steps)
-    parser.add_argument("--num_iterations", type=int, default=10, help="number of iterations to run")
+    parser.add_argument("--num_iterations", type=int, default=9536, help="number of iterations to run")
     # optimization
-    parser.add_argument("--learning_rate", type=float, default=1e-4, help="learning rate warmup iterations")
-    parser.add_argument("--warmup_iters", type=int, default=0, help="learning rate warmup iterations")
-    parser.add_argument("--warmdown_iters", type=int, default=0, help="learning rate warmdown iterations")
-    parser.add_argument("--weight_decay", type=float, default=0.0, help="weight decay")
+    parser.add_argument("--learning_rate", type=float, default=0.0018, help="learning rate warmup iterations")
+    parser.add_argument("--warmup_iters", type=int, default=256, help="learning rate warmup iterations")
+    parser.add_argument("--warmdown_iters", type=int, default=2048, help="learning rate warmdown iterations")
+    parser.add_argument("--weight_decay", type=float, default=0.1, help="weight decay")
     # evaluation
-    parser.add_argument("--val_loss_every", type=int, default=0, help="every how mant steps to evaluate val loss?")
+    parser.add_argument("--val_loss_every", type=int, default=128, help="every how mant steps to evaluate val loss?")
     parser.add_argument("--val_max_steps", type=int, default=20, help="how many batches of val to average?")
     parser.add_argument("--save_every", type=int, default=5000, help="every how many steps to save the checkpoint")
     args = parser.parse_args()
+
+    args.input_bin = os.path.expandvars(args.input_bin)
+    args.input_val_bin = os.path.expandvars(args.input_val_bin)
+    args.output_dir = os.path.join(os.path.expandvars(args.output_dir), time.strftime("%Y%m%d-%H%M%S"))
 
     # args error checking and convenience variables
     B, T = args.batch_size, args.sequence_length
