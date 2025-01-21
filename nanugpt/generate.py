62c5cc5545e19397d23d65c9926828098abd48e6
Shital Shah <sytelus@gmail.com>
2025-01-21 03:40:04 -0800

    generate bug fix

--- nanugpt/generate.py
+++ nanugpt/generate.py
@@ -91,9 +91,9 @@ class Generator:
                 # if the sequence context is growing too long we must crop it at block_size
                 idx_cond = idx if idx.size(1) <= self.context_length else idx[:, -self.context_length:]
                 # forward the model to get the logits for the index in the sequence
-                logits = self.model(idx_cond, only_last=True)
+                logits, *_ = self.model(idx_cond, only_last=True)
                 # pluck the logits at the final step and scale by desired temperature
-                logits = logits[:, [-1], :] / temperature
+                logits = logits[:, -1, :] / temperature
                 # optionally crop the logits to only the top k options
                 if top_k is not None:
                     v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
