1807f141248fe3714209a8299f9a900abaca1603
Shital Shah <sytelus@gmail.com>
2025-02-13 02:43:41 -0800

    getting past compile

--- nanugpt/data/arithmatic_data.py
+++ nanugpt/data/arithmatic_data.py
@@ -11,27 +11,27 @@ from torch.utils.data import Dataset
 from nanugpt import utils
 from nanugpt.tokenizers.arithmatic_tokenizer import get_tokenizer_factory, ArithmaticTokenizer
 
-def _pack_sequences(docs: List[str], context_len: int, pad: str) -> List[str]:
+def _pack_sequences(docs: List[str], context_length: int, pad: str) -> List[str]:
     sequences = []
     current_seq = ""
     for doc in docs:
-        if len(current_seq) + len(doc) <= context_len:
+        if len(current_seq) + len(doc) <= context_length:
             current_seq += doc
         else:
-            needed = context_len - len(current_seq)
+            needed = context_length - len(current_seq)
             pad_str = (pad * ((needed // len(pad)) + 1))[:needed]
             sequences.append(current_seq + pad_str)
             current_seq = doc
-        if len(current_seq) == context_len:
+        if len(current_seq) == context_length:
             sequences.append(current_seq)
             current_seq = ""
     if current_seq:
-        needed = context_len - len(current_seq)
+        needed = context_length - len(current_seq)
         pad_str = (pad * ((needed // len(pad)) + 1))[:needed]
         sequences.append(current_seq + pad_str)
     return sequences
 
-def _process_n_digits(n_digits: int, max_samples: int, context_len: int,
+def _process_n_digits(n_digits: int, max_samples: int, context_length: int,
                       op: str, eq: str, sep: str, pad: str,
                       seed:int) -> Tuple[List[str], List[str]]:
     low = 10**(n_digits - 1)
@@ -55,21 +55,21 @@ def _process_n_digits(n_digits: int, max_samples: int, context_len: int,
     docs = np.char.add(np.char.add(np.char.add(np.char.add(x_str, op), y_str), eq), z_str)
     docs = np.char.add(docs, sep)
     docs_list:List[str] = docs.tolist() # type: ignore
-    sequences = _pack_sequences(docs_list, context_len, pad)
+    sequences = _pack_sequences(docs_list, context_length, pad)
 
     if total_count <= max_samples:
         return sequences, []
     else:
         return sequences[::2], sequences[1::2]
 
-def gen_mul_seqs(min_digits: int, max_digits: int, max_samples: int, context_len: int,
+def gen_mul_seqs(min_digits: int, max_digits: int, max_samples: int, context_length: int,
                  op: str, eq: str, sep: str, pad: str, seed:int) -> Tuple[List[str], List[str]]:
     train_sequences = []
     test_sequences = []
 
     # Create a partial function that fixes the parameters except n_digits.
     process_func = partial(_process_n_digits, max_samples=max_samples,
-                           context_len=context_len, op=op, eq=eq, sep=sep, pad=pad,
+                           context_length=context_length, op=op, eq=eq, sep=sep, pad=pad,
                            seed=seed)
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
@@ -87,7 +87,7 @@ def shift_by_1(tensor: torch.Tensor, eos_token_id: int) -> torch.Tensor:
     return shifted
 
 def get_data(device_batch_size: int, eval_batch_size:int, data_loader_seed:int,
-             min_digits: int, max_digits: int, max_samples: int, context_len: int)->Tuple[DataLoader,DataLoader, Optional[DataLoader]]:
+             min_digits: int, max_digits: int, max_samples: int, context_length: int)->Tuple[DataLoader,DataLoader, Optional[DataLoader]]:
 
     world_size = utils.get_world_size()
     global_rank = utils.get_global_rank()
@@ -98,12 +98,12 @@ def get_data(device_batch_size: int, eval_batch_size:int, data_loader_seed:int,
 
     seed = data_loader_seed + global_rank
 
-    train_strings, val_strings = gen_mul_seqs(min_digits, max_digits, max_samples, context_len,
+    train_strings, val_strings = gen_mul_seqs(min_digits, max_digits, max_samples, context_length,
                            op='*', eq='=',
                            sep=tokenizer.eos_str, pad=tokenizer.pad_str,
                            seed=seed)
-    train_tensors = tokenizer.strings2tensor(train_strings)
-    val_tensors = tokenizer.strings2tensor(val_strings)
+    train_tensors = tokenizer.strings2tensor(train_strings).to(torch.int)
+    val_tensors = tokenizer.strings2tensor(val_strings).to(torch.int)
 
     train_dataset = torch.utils.data.TensorDataset(train_tensors, shift_by_1(train_tensors, tokenizer.eos_token_id))
     val_dataset = torch.utils.data.TensorDataset(val_tensors, shift_by_1(val_tensors, tokenizer.eos_token_id))
