99c40fe5285e2f2856f8560b1bcb7f98122351d0
Shital Shah <sytelus@gmail.com>
2025-09-13 03:34:00 +0000

    fix: update RUN_NAME and clean up environment variables in train_nanugpt script

--- scripts/volcano/train_nanugpt.sh
+++ scripts/volcano/train_nanugpt.sh
@@ -6,7 +6,7 @@ set -eu -o pipefail # fail if any command failes, log all commands, -o xtrace
 # setup below values
 
 # can't inline these vars because we are using it as parameter to submit script
-export RUN_NAME=nanugpt-openwebtext-b60
+export RUN_NAME=nanugpt-openwebtext-full
 export JOB_NAME=gpt-std
 
 # directory where this script is running
@@ -14,9 +14,9 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 export OUT_DIR=/data/shitals
 export DATA_ROOT=/data/shitals/data
-export TORCHINDUCTOR_AUTOTUNE_IN_SUBPROC=1
+# export TORCHINDUCTOR_AUTOTUNE_IN_SUBPROC=1
 
-export TRANSFER_VARS="DATA_ROOT WANDB_API_KEY WANDB_HOST TORCHINDUCTOR_AUTOTUNE_IN_SUBPROC"
+export TRANSFER_VARS="DATA_ROOT WANDB_API_KEY WANDB_HOST"
 
 NODES=1 \
 bash ${SCRIPT_DIR}/vsubmit.sh train.py configs/train_gpt2/openwebtext.yaml --general.project_name nanugpt-openwebtext --general.run_name ${RUN_NAME} $@
