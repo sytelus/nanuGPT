e7212bff676614458aa8636eb82acfcb50b8d715
Shital Shah <sytelus@gmail.com>
2025-09-14 02:26:48 +0000

    added cooldown_frac

--- nanugpt/schedulers/constant.py
+++ nanugpt/schedulers/constant.py
@@ -7,19 +7,23 @@ from nanugpt import glogging as logging
 
 class ConstantWithCooldownScheduler(LRScheduler):
     def __init__(self, optimizer, const_lr:float,
-                 warmup_iters: int, max_iters: Optional[int], cooldown_iters: int,
+                 warmup_iters: Optional[int], max_iters: Optional[int],
+                 cooldown_iters: Optional[int], cooldown_frac:Optional[float],
                  end_factor: float,
                  last_epoch=-1):
 
         self.const_lr = const_lr
 
         self.warmup_iters = warmup_iters
-        self.cooldown_iters = cooldown_iters
         self.end_factor = end_factor
         self.max_iters = max_iters
+        self.cooldown_iters = cooldown_iters
 
-        assert (self.max_iters is None and self.cooldown_iters == 0) or (self.max_iters is not None and self.cooldown_iters >= 0), "max_iters must be specified if cooldown_iters > 0"
-        assert self.max_iters is None or warmup_iters + cooldown_iters <= self.max_iters, f"warmup_iters + cooldown_iters {warmup_iters}+{cooldown_iters} must be less than max_iters {self.max_iters}"
+        if cooldown_frac is not None:
+            assert max_iters is not None, "max_iters must be specified if cooldown_frac is specified"
+            assert cooldown_iters is None, "Only one of cooldown_iters or cooldown_frac should be specified"
+            self.cooldown_iters = int(cooldown_frac * max_iters)
+
         super().__init__(optimizer, last_epoch)
 
     def get_lr(self):
@@ -30,14 +34,15 @@ class ConstantWithCooldownScheduler(LRScheduler):
         init_lrs = np.fromiter((group['initial_lr'] for group in self.optimizer.param_groups), dtype=np.float32)
 
          # linear warmup for warmup_iters steps
-        if self.last_epoch < self.warmup_iters:
+        if self.warmup_iters is not None and self.last_epoch < self.warmup_iters:
             return (init_lrs * self.last_epoch / self.warmup_iters).tolist()
 
+        # if we ran past max_iters, return min LR
         if self.max_iters is not None and self.last_epoch >= self.max_iters: # return min LR
             return (init_lrs * self.end_factor).tolist()
 
         # if within cooldown, return linear decay down to min learning rate
-        if self.max_iters is not None and self.last_epoch > (self.max_iters - self.cooldown_iters):
+        if self.cooldown_iters is not None and self.max_iters is not None and self.last_epoch > (self.max_iters - self.cooldown_iters):
             min_lr = init_lrs * self.end_factor
             decay_ratio = (self.max_iters - self.last_epoch - 1) / self.cooldown_iters
             return ((init_lrs - min_lr) * decay_ratio + min_lr).tolist()
@@ -47,7 +52,8 @@ class ConstantWithCooldownScheduler(LRScheduler):
 
 
 def get_scheduler(optimizer, const_lr:float,
-                  warmup_iters: int, max_iters: Optional[int], cooldown_iters: int,
+                  warmup_iters: Optional[int], max_iters: Optional[int],
+                  cooldown_iters: Optional[int], cooldown_frac:Optional[float],
                   end_factor:float):
     return ConstantWithCooldownScheduler(
             optimizer=optimizer,
@@ -55,5 +61,6 @@ def get_scheduler(optimizer, const_lr:float,
             warmup_iters=warmup_iters,
             max_iters=max_iters,
             cooldown_iters=cooldown_iters,
+            cooldown_frac=cooldown_frac,
             end_factor=end_factor,
         )
