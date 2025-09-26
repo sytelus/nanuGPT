3748fb3bac4c417cfe915110a68596b362cfdc66
Shital Shah <sytelus@gmail.com>
2025-09-26 23:34:12 +0000

    added back gpu devbox

--- scripts/volcano/vdevbox.sh
+++ scripts/volcano/vdevbox.sh
@@ -31,39 +31,46 @@ export NCCL_IB_DISABLE=${NCCL_IB_DISABLE:-0}
 export OMP_NUM_THREADS=${OMP_NUM_THREADS:-8}
 
 if [ "$#" -eq 0 ]; then
-#   # default GPU devbox vars
-#   export JOB_NAME=${USER_ALIAS}-devbox
-#   export GPUS_PER_NODE=${GPUS_PER_NODE:-8}
-#   export CONTAINER_IMAGE_PATH=${CONTAINER_IMAGE_PATH:-"nvcr.io/nvidia/pytorch:25.08-py3"} #docker://@nvcr.io#nvidia/pytorch:24.07-py3
+  # prompt user that GPU devboxes are expensive and should not be kept alivve beyond short duration and
+  # if they agree with responsible usage then proceed else ask them to use --cpu switch and exit
+  echo "You are about to start a GPU devbox. This is expensive and MUST not be kept alive beyond short duration."
+  read -p "Are you aware about responsible use guidelines for this cluster? (y/n): " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      echo "Please use --cpu switch to start a CPU only devbox. Contact Shital or Sahaj for learning more about responsible usage."
+      exit 1
+  fi
 
-#   export MEMORY_SIZE_LIMIT=${MEMORY_SIZE_LIMIT:-100Gi}
-#   export CPU_REQUESTS=${CPU_REQUESTS:-192}
-#   export MEMORY_REQUESTS=${MEMORY_REQUESTS:-2600Gi}
-#   export RDMA_REQUESTS=${RDMA_REQUESTS:-1}
+  # default GPU devbox vars
+  export JOB_NAME=${USER_ALIAS}-devbox
+  export GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+  export CONTAINER_IMAGE_PATH=${CONTAINER_IMAGE_PATH:-"nvcr.io/nvidia/pytorch:25.08-py3"} #docker://@nvcr.io#nvidia/pytorch:24.07-py3
 
-#   # good defaults for Pytorch
-#   # avoid OOM errors by allowing segments to expand
-#   export PYTORCH_CUDA_ALLOC_CONF=${PYTORCH_CUDA_ALLOC_CONF:-expandable_segments:True}
-#   # turn on heavy optimizations in torchinductor
-#   export TORCHINDUCTOR_COORDINATE_DESCENT_TUNING=${TORCHINDUCTOR_COORDINATE_DESCENT_TUNING:-1}
+  export MEMORY_SIZE_LIMIT=${MEMORY_SIZE_LIMIT:-100Gi}
+  export CPU_REQUESTS=${CPU_REQUESTS:-192}
+  export MEMORY_REQUESTS=${MEMORY_REQUESTS:-2600Gi}
+  export RDMA_REQUESTS=${RDMA_REQUESTS:-1}
 
-#   export TOLERENCE_YAML='tolerations:
-#             - key: "nvidia.com/gpu"
-#               operator: "Exists"
-#               effect: "NoSchedule"
-# '
-#   # if RDMA_REQUESTS is 1 then add nvidia gpu toleration as well
-#   if [[ "${RDMA_REQUESTS}" -eq 1 ]]; then
-#     TOLERENCE_YAML+='
-#             - key: "rdma"
-#               operator: "Exists"
-#               effect: "NoSchedule"
-# '
-#   fi
-#   export RDMA_YAML="rdma/rdma_shared_device_a: \"${RDMA_REQUESTS}\""
-  echo "GPU devbox is not permitted on this cluster, use --cpu option" >&2
-  echo "Usage: $0 [--cpu]" >&2
-  exit 1
+  # good defaults for Pytorch
+  # avoid OOM errors by allowing segments to expand
+  export PYTORCH_CUDA_ALLOC_CONF=${PYTORCH_CUDA_ALLOC_CONF:-expandable_segments:True}
+  # turn on heavy optimizations in torchinductor
+  export TORCHINDUCTOR_COORDINATE_DESCENT_TUNING=${TORCHINDUCTOR_COORDINATE_DESCENT_TUNING:-1}
+
+  export TOLERENCE_YAML='tolerations:
+            - key: "nvidia.com/gpu"
+              operator: "Exists"
+              effect: "NoSchedule"
+'
+  # if RDMA_REQUESTS is 1 then add nvidia gpu toleration as well
+  if [[ "${RDMA_REQUESTS}" -eq 1 ]]; then
+    TOLERENCE_YAML+='
+            - key: "rdma"
+              operator: "Exists"
+              effect: "NoSchedule"
+'
+  fi
+  export RDMA_YAML="rdma/rdma_shared_device_a: \"${RDMA_REQUESTS}\""
 else
   if [[ "$1" == "--cpu" ]]; then
     # CPU only devbox
